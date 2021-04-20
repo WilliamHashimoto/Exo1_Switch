8e9eff6a93d9d71b33ab0e7088d451c691f67fa8
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-20 16:53:00 +0200

    fini

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+let day = prompt("Donnez un jour de la semaine:");
+
+switch (day) {
+    case "lundi":
+        console.log("premier jour de confinement");
+        break;
+
+    case "mardi":
+        console.log("deuxieme jour de confinement");
+        break;
+
+    case "mercredi":
+        console.log("troisieme jour de confinement");
+        break;
+
+    case "jeudi":
+        console.log("quatrieme jour de confinement");
+        break;
+
+    case "vendredi":
+        console.log("cinquieme jour de confinement");
+        break;
+
+    case "samedi":
+        console.log("sixieme jour de confinement");
+        break;
+    
+    case "dimanche":
+        console.log("septieme jour de confinement");
+        break;
+
+    default:
+        break;
+}
